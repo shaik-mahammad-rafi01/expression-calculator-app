725ef00d7cc7d8222bf90ac019275fceff380489
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-07-27 21:35:51 +0530

    Add subtraction test case

--- tests/expression_calculator.test.ts
+++ tests/expression_calculator.test.ts
@@ -4,5 +4,8 @@ describe("Basic Operations",()=>{
     test("addition of 2 numbers",()=>{
         expect(addition(2,1)).toBe(3)
     })
+    test("subraction of 2 numbers",()=>{
+        expect(subtraction(3,1)).toBe(2)
+    })
 })
 
