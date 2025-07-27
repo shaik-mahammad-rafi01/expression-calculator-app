a045c5c31f03ea23ebbebfac16ff627577839cc3
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-07-27 22:26:05 +0530

    Add test:addition of one positive & negative number

--- tests/expression_calculator.test.ts
+++ tests/expression_calculator.test.ts
@@ -1,28 +1,33 @@
 import { re } from "mathjs"
-import { addition , subtraction , multiplication , division ,modulus, powercalculation} from "../src/expression_calculator"
-describe("Basic Operations",()=>{
+import { addition, subtraction, multiplication, division, modulus, powercalculation } from "../src/expression_calculator"
+describe("Basic Operations", () => {
 
-    test("addition of 2 numbers",()=>{
-        expect(addition(2,1)).toBe(3)
-    })
-    test("subraction of 2 numbers",()=>{
-        expect(subtraction(3,1)).toBe(2)
-    })
-    test("Multiplication of 2 numbers",()=>{
-        expect(multiplication(2,3)).toBe(6)
+    test("addition of 2 numbers", () => {
+        expect(addition(2, 1)).toBe(3)
     })
 
-    test("Division of 2 numbers",()=>{
-        expect(division(4,2)).toBe(2)
+    test("add a positive and a negative number",()=>{
+        expect(addition(5, -2)).toBe(3);
+    });
+
+    test("subraction of 2 numbers", () => {
+        expect(subtraction(3, 1)).toBe(2)
     })
-    test("division by zero",()=>{
-        expect(()=>division(2,0)).toThrow("can not divide by zero");
+    test("Multiplication of 2 numbers", () => {
+        expect(multiplication(2, 3)).toBe(6)
     })
-    test("Modulus",()=>{
-        expect(modulus(5,2)).toBe(1);
+
+    test("Division of 2 numbers", () => {
+        expect(division(4, 2)).toBe(2)
     })
-    test("power calculation",()=>{
-        expect(powercalculation(2,2)).toBe(4)
+    test("division by zero", () => {
+        expect(() => division(2, 0)).toThrow("can not divide by zero");
+    })
+    test("Modulus", () => {
+        expect(modulus(5, 2)).toBe(1);
+    })
+    test("power calculation", () => {
+        expect(powercalculation(2, 2)).toBe(4)
     })
 })
 
