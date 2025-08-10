b7152cf89f76a3aba338be26bedba8c65f5c01c6
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-08-10 20:14:22 +0530

    update jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
- module.exports = {
-  preset: "ts-jest",
+const { createDefaultPreset } = require("ts-jest");
+const tsJestTransformCfg = createDefaultPreset().transform;
+/** @type {import("jest").Config} **/
+module.exports = {
   testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  collectCoverageFrom: ["src/**/*.ts"],
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage',
-  coverageReporters: ['text', 'lcov'],
+  transform: {
+    ...tsJestTransformCfg,
+  },
   coverageThreshold: {
     global: {
       branches: 90,
       functions: 90,
-      lines: 100,
-      statements: 90
-    }
-  }
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
